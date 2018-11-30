5ee2999f82a0a98a1e81de6c8e8610a3fba00b54
Andrew Minion <andrew@andrewrminion.com>
2018-11-29 21:45:15 -0500

    initial plugin

---
+++ cf7-allow-mailgun-validation.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * Plugin Name: Contact Form 7 Allow Mailgun Domain Validation
+ * Description: Allows the mg subdomain for your site to pass Contact Form 7 email validation.
+ * Version: 1.0.0
+ * Author: AndrewRMinion Design
+ * Author URI: https://andrewrminion.com.com
+ *
+ * @package cf7-allow-mailgun-validation
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * CF7 Mailgun validation.
+ */
+class CF7_Allow_Mailgun_Validation {
+	/**
+	 * Class instance.
+	 *
+	 * @var CF7_Allow_Mailgun_Validation
+	 */
+	private static $_instance = null;
+
+	/**
+	 * Return only one instance of this class.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return CF7_Allow_Mailgun_Validation class.
+	 */
+	public static function get_instance() {
+		if ( null === self::$_instance ) {
+			self::$_instance = new CF7_Allow_Mailgun_Validation();
+		}
+
+		return self::$_instance;
+	}
+
+	/**
+	 * Kick things off.
+	 *
+	 * @since 1.0.0
+	 */
+	public function __construct() {
+		add_action( 'wpcf7_config_validator_validate', array( $this, 'validate' ) );
+	}
+
+	/**
+	 * Allow the mg subdomain of your site to pass validation.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param WPCF7_ConfigValidator $validator Instance of the CF& config validator.
+	 *
+	 * @return void
+	 */
+	public function validate( $validator ) {
+
+		// Get fields to search.
+		$search_fields = array(
+			'mail.sender',
+			'mail2.sender',
+		);
+
+		// Get site domain.
+		$mg_domain = 'mg.' . str_replace( 'http://', '', get_site_url( null, '', 'http' ) );
+
+		/**
+		 * Filter the Mailgun domain.
+		 *
+		 * @param string $domain Your mailgun domain; defaults to 'mg'.
+		 *
+		 * @return string Your mailgun domain.
+		 */
+		$mg_domain = apply_filters( 'cf7_mailgun_validation', $mg_domain );
+
+		// Get contact form and errors.
+		$form       = $validator->contact_form();
+		$form_props = $form->get_properties();
+		$errors     = $validator->collect_error_messages();
+
+		// Check each error.
+		foreach ( $errors as $key => $error ) {
+			$key_parts = explode( '.', $key );
+
+			if ( in_array( $key, $search_fields, true ) ) {
+				foreach ( $errors[ $key ] as $sub_key => $sub_error ) {
+
+					$sender_string = $form_props[ $key_parts[0] ][ $key_parts[1] ];
+					$senders       = explode( ',', $sender_string );
+
+					// Check all senders.
+					foreach ( $senders as $sender ) {
+						if ( false !== strpos( $sender, '@' . $mg_domain ) && 'Sender email address does not belong to the site domain.' === $sub_error['message'] ) {
+							$validator->remove_error( $key, 103 );
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+CF7_Allow_Mailgun_Validation::get_instance();
---
+++ readme.txt
@@ -0,0 +1,41 @@
+=== Contact Form 7 Mailgun Domain Validation ===
+Contributors: macbookandrew
+Tags: contact form, contact form 7, cf7, contactform7, validation, mailgun
+Donate link: https://cash.me/$AndrewRMinionDesign
+Requires at least: 4.8
+Tested up to: 5.0.0
+Stable tag: 1.0.0
+License: GPL2
+
+Allows email addresses using your site’s Mailgun domain to pass Contact Form 7’s form validation feature.
+
+== Description ==
+Do you use Mailgun to deliver emails from your WordPress site? Does it annoy you that the Mailgun email address causes Contact Form 7 validation to fail?
+
+This plugin allows email addresses using your site’s Mailgun domain to pass Contact Form 7’s form validation feature.
+
+It also includes the `cf7_mailgun_domain` filter you can use to modify the default 'mg' domain. Example:
+
+```
+/**
+ * Filter the Mailgun domain.
+ *
+ * @param string $domain Your mailgun domain; defaults to 'mg.your-site.com'.
+ *
+ * @return string Your mailgun domain.
+ */
+function my_custom_mailgun_domain( $domain ) {
+	$domain = 'mg.my-site.com'; // Replace this with your Mailgun domain.
+	return $domain;
+}
+add_filter( 'cf7_mailgun_domain', 'my_custom_mailgun_domain' );
+```
+
+== Installation ==
+1. Install and activate the plugin
+1. Revalidate your Contact Form 7 forms.
+
+== Changelog ==
+
+= 1.0 =
+- First stable version
